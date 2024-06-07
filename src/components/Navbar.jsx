5849c5c4acf5d29a57cd5ac4d2d554d82d77f5b3
Sandia <sandiacreativecrew@gmail.com>
2024-06-07 17:31:35 +0200

    format Navbar and add poges folder

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,34 @@
-import React from 'react'
+import React from "react";
 
 function Navbar() {
   return (
     <>
-    <nav>
-        <ul className='w-full flex justify-center p-2 bg-zinc-900 gap-3 '>
-            <li className='text-zinc-200 bg-zinc-800 rounded'><a className='p-3 flex text-center w-full h-full' href="#">Home</a></li>
-            <li className='text-zinc-200 bg-zinc-800 rounded'><a className='p-3 flex text-center w-full h-full' href="#">Scan QR</a></li>
-            <li className='text-zinc-200 bg-zinc-800 rounded'><a className='p-3 flex text-center w-full h-full' href="#">Add Client</a></li>
-            <li className='text-zinc-200 bg-zinc-800 rounded'><a className='p-3 flex text-center w-full h-full' href="#">Clients</a></li>
+      <nav>
+        <ul className="w-full flex justify-center p-2 bg-zinc-900 gap-3 ">
+          <li className="text-zinc-200 bg-zinc-800 rounded">
+            <a className="p-3 flex text-center w-full h-full" href="#">
+              Home
+            </a>
+          </li>
+          <li className="text-zinc-200 bg-zinc-800 rounded">
+            <a className="p-3 flex text-center w-full h-full" href="#">
+              Scan QR
+            </a>
+          </li>
+          <li className="text-zinc-200 bg-zinc-800 rounded">
+            <a className="p-3 flex text-center w-full h-full" href="#">
+              Add Client
+            </a>
+          </li>
+          <li className="text-zinc-200 bg-zinc-800 rounded">
+            <a className="p-3 flex text-center w-full h-full" href="#">
+              Clients
+            </a>
+          </li>
         </ul>
-    </nav>
+      </nav>
     </>
-  )
+  );
 }
 
-export default Navbar
-
+export default Navbar;
